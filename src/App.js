96e83a572d19201f2ee470f6043497447d402e3f
moshtaba <darzimojtaba@gmail.com>
2019-02-06 13:29:02 +0330

    home responsive and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 import Home from './container/home/Home'
 
 import './App.css';
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Home />
+        <Footer />
       </div>
     );
   }
