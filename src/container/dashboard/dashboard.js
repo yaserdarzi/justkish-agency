02945070a08d780f6a49179c371c7aaf92d39ddf
moshtaba <darzimojtaba@gmail.com>
2019-02-06 20:36:22 +0330

    responsive dashboard step1

--- src/container/dashboard/dashboard.js
+++ src/container/dashboard/dashboard.js
@@ -1,4 +1,13 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+
+
+import Seller from './../../components/seller/Seller'
+
+import report from './../../../assets/icons/report.svg'
+import support from './../../../assets/icons/support.svg'
+import call from './../../../assets/icons/call.svg'
+import mail from './../../../assets/icons/mail.svg'
+import questionmark from './../../../assets/icons/questionmark.svg'
 
 import './dashboard.css';
 
@@ -7,43 +16,87 @@ import './dashboard.css';
 class Dashboard extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
+    render() {
+        return (
             <div className="dashboard">
-                <div className="part-1">
-           
+                <div className="dashboard1">
+                    <div className="calender-weather-box" >
+                        <div className="dashboard-calenrder" ></div>
+                        <div className="dashboard-weather" ></div>
+                    </div>
+                    <div className="sale-statistics" >
+                        <div className="statistics-actions" >
+                            <div className="sale-title" >
+                                <h3>فروش<img src={questionmark} alt="فروش" /></h3>
+                                <span>آمار کل فروش در سامانه </span>
+                            </div>
+                            <div className="dashboard-dates" >
+                                <ul>
+                                    <li className="time-active">هفته</li>
+                                    <li>ماه</li>
+                                    <li>سال</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="dashboard-chart" >
+
+
+                        </div>
+                    </div>
+
                 </div>
-                <div className="part-2">
+                <div className="dashboard2">
                     <div className="dashbord-sellers">
-                        <p>حساب کاربری شما</p>
+                        <p className="dashbord-sellers-title" >عاملین فروش<img src={questionmark} alt="فروش" /></p>
                         <ul className="dashbord-manage-sellers" >
-                            <li className="dashbord-manage-sellers-item">
-                                <div className="dashboard-seller-actions">
-                                    <div className="dashboard-seller-actions-item"></div>
-                                    <div className="dashboard-seller-actions-item"></div>
-                                </div>
-                                <div className="dashboard-seller-item-container">
-                                    <p>Name Seller</p>
-                                    <span>عامل فروش</span>
-                                </div>
-                                <div className="dashboard-seller-avatar"></div>
-                            </li>
-                            <li className="dashbord-manage-sellers-item"></li>
-                            
+
+                            <Seller name="MOJTABA" level="عاملین فروش" />
+                            <Seller name="ARAS" level="عاملین فروش" />
+                            <Seller name="YAER" level="عاملین فروش" />
+
                         </ul>
 
                         <button className="checkout-request" >
-                          اضافه کردن عامل فروش جدید
+                            اضافه کردن عامل فروش جدید
                         </button>
 
 
-                    </div> 
+                    </div>
+                    <div className="dashboard-report" >
+                        <img src={report} alt="گزارش مالی" />
+                        <span>گزارش فروش ومالی</span>
+                    </div>
+                    <div className="dashboard-support">
+                        <h1><img src={support} alt="پشتیبانی" /> پشتیبانی</h1>
+                        <div className="dashboard-support-text" >
+                            <h2>پشتیبانی فنی</h2>
+                            <p className="dashboard-support-phone">
+                            ‍‍      <span>076-44495698 -- 076-44495698</span>
+                                <img src={call} alt="تلفن" />
+                            </p>
+                            <p className="dashboard-support-mail">
+                                ‍‍     <span>support@justkish.com</span>
+                                <img src={mail} alt="ایمیل" />
+                            </p>
+                        </div>
+                        <div className="dashboard-support-text" >
+                            <h2>پشتیبانی فروش در جزیرخ کیش</h2>
+                            <p className="dashboard-support-phone">
+                                ‍‍      <span>076-44495698 -- 076-44495698</span>
+                                <img src={call} alt="تلفن" />
+                            </p>
+                            <p className="dashboard-support-mail" >
+                                ‍‍     <span>support@justkish.com</span>
+                                <img src={mail} alt="ایمیل" />
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
-         );
+        );
     }
 }
- 
+
 export default Dashboard;
