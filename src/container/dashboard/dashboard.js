334d770c92791b24cb388118b60bc91da89af073
aras armani <aras.armani2014@gmail.com>
2019-02-06 14:52:49 +0300

    add all

--- src/container/dashboard/dashboard.js
+++ src/container/dashboard/dashboard.js
@@ -11,14 +11,36 @@ class Dashboard extends Component {
     }
     render() { 
         return ( 
-            <div>
+            <div className="dashboard">
                 <div className="part-1">
-                    <div className="sellers">
+           
+                </div>
+                <div className="part-2">
+                    <div className="dashbord-sellers">
                         <p>حساب کاربری شما</p>
-                        <ul className="manage-sellers" ></ul>
+                        <ul className="dashbord-manage-sellers" >
+                            <li className="dashbord-manage-sellers-item">
+                                <div className="dashboard-seller-actions">
+                                    <div className="dashboard-seller-actions-item"></div>
+                                    <div className="dashboard-seller-actions-item"></div>
+                                </div>
+                                <div className="dashboard-seller-item-container">
+                                    <p>Name Seller</p>
+                                    <span>عامل فروش</span>
+                                </div>
+                                <div className="dashboard-seller-avatar"></div>
+                            </li>
+                            <li className="dashbord-manage-sellers-item"></li>
+                            
+                        </ul>
+
+                        <button className="checkout-request" >
+                          اضافه کردن عامل فروش جدید
+                        </button>
+
+
                     </div> 
                 </div>
-                <div className="part-2"></div>
             </div>
          );
     }
