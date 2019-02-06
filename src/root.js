9c19d200fa9650b32fb161649aca6b9926d06abb
aras armani <aras.armani2014@gmail.com>
2019-02-06 12:48:19 +0300

    index fixed

--- src/root.js
+++ src/root.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 // import Header from "./header/header";
 // import Footer from "./footer/footer";
+import IndexComponent from "./container/beforLogin/inddex/index";
 import HeaderComponentAfterLogin from './components/header/Header';
 
 // import Token from './api/token';
 
-
+// const Token = 'Login'
+const Token = 'logout'
  
 export default class Root extends Component {
     constructor(props) {
@@ -26,11 +28,13 @@ export default class Root extends Component {
         return ( 
             <div>
                 <div>
-                    {/* {Token === 'null' ? ( <Header /> ): ( <HeaderComponentAfterLogin/> )}             */}
-                    <HeaderComponentAfterLogin/>
+                    {Token === 'logout' ? ( '' ): ( <HeaderComponentAfterLogin/> )}            
+                   
                 </div>
                 <div> 
-                    {this.props.children}
+                    
+                {Token === 'logout' ? ( <IndexComponent /> ): ( this.props.children )}            
+                   
                 </div>
                 <div>
                     {/* <Footer /> */}
