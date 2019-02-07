ff5e1bb57c88a5d5a930ed699fd3262be9e59af0
moshtaba <darzimojtaba@gmail.com>
2019-02-07 11:43:52 +0330

    wallet 1

---
+++ src/container/wallet/Wallet.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+
+import './Wallet.css';
+
+
+class Wallet extends Component {
+    render() {
+        return (
+            <div className="Wallet">
+                <div className="Wallet-box container" >
+
+                    <div className="increase-credit" >
+                       
+                    </div>
+                    <div className="transaction" >
+                       
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Wallet;
