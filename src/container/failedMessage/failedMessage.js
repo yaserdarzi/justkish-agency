1cdd51891735102c481cccc63f13c45961a00156
aras armani <aras.armani2014@gmail.com>
2019-02-20 15:51:23 +0300

    fixd by success message

--- src/container/failedMessage/failedMessage.js
+++ src/container/failedMessage/failedMessage.js
@@ -5,13 +5,41 @@ import './failedMessage.css'
 class FailedMessage extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            tokenMessage:''
+         }
     }
+
+
+
+    componentWillMount(){
+        this.setState({
+          tokenMessage:  this.getParms('token')
+        }) 
+  
+      }
+  
+  
+      getParms(value) {
+  
+          let url_string = window.location.href
+          let url = new URL(url_string);
+  
+          const val = url.searchParams.get(value);
+         // console.log(val)
+          if (val !== null)
+              return val;
+          return 0
+      }
+
+
+
     render() { 
         return ( 
             <div className="failed-message-payment">
             <div className="failed-message-box bounceIn">
                  <h1>پرداخت ناموفق</h1>
+                 <h2>{this.state.tokenMessage}</h2>
                 <p>درخواست پرداخت شما از میزان بستانکاری شما بیشتر است و افزایش اعتبار شما با شکست مواجه شد</p>
 
                  <Button                                                                  
