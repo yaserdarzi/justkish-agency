e786f4d6f2d340e7ea5fde20b24326afa989d4eb
aras armani <aras.armani2014@gmail.com>
2019-04-01 23:47:06 +0300

    get date todey is fixed

--- src/container/ticketIssued/TicketIssued.js
+++ src/container/ticketIssued/TicketIssued.js
@@ -134,8 +134,10 @@ class TicketIssued extends Component {
                     <p className="ticket-issued-search-list-cell-1">{item.id}</p>
                     <p className="ticket-issued-search-list-cell">{item.products.title ? item.products.title : item.tours.title}</p>
                     <p className="ticket-issued-search-list-cell">
+                     <div style={{display:'flex', flexDirection:'column'}}>
                         {MiladiToJalaly(TimeStamp(item.products_episode.start_date))}
                         <p>{item.products_episode.start_hours} - {item.products_episode.end_hours}</p>
+                     </div>
                     </p>
                     <p className="ticket-issued-search-list-cell">{item.name}</p>
                     <p className="ticket-issued-search-list-cell">{item.phone}</p>
@@ -169,7 +171,7 @@ class TicketIssued extends Component {
                         <div className="ticket-issued-search-lists table-desktop" >
                             <div className="ticket-issued-search-list-titles" >
                                 <p className="ticket-issued-search-list-title-1">شماره </p>
-                                <p className="ticket-issued-search-list-title">نام حصول</p>
+                                <p className="ticket-issued-search-list-title">نام محصول</p>
                                 <p className="ticket-issued-search-list-title">سانس</p>
                                 <p className="ticket-issued-search-list-title">نام خریدار</p>
                                 <p className="ticket-issued-search-list-title">شماره تماس</p>
