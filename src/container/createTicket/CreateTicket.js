d2326660e69fc7f6d50dd4093cfcfed3fd5fa744
moji0098 <darzimojtaba@gmail.com>
2019-04-13 12:28:14 +0430

    mouse hover enter hobies

--- src/container/createTicket/CreateTicket.js
+++ src/container/createTicket/CreateTicket.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {  browserHistory } from 'react-router';
+import { browserHistory } from 'react-router';
 import { DateRangePicker, DatePicker } from "react-advance-jalaali-datepicker";
 /*
 
@@ -60,11 +60,11 @@ class CreateTicket extends Component {
             categorie: this.getParms('categories') || 0,
             person: 0,
             getShoping: true,
-            customerName:'',
-            customerPhone:'',
-            customerNameError:'',
-            customerPhoneError:'',
-            isLoadingPayment:false,
+            customerName: '',
+            customerPhone: '',
+            customerNameError: '',
+            customerPhoneError: '',
+            isLoadingPayment: false,
             currentDate: ''
         }
     }
@@ -76,12 +76,12 @@ class CreateTicket extends Component {
         this.getAllShopingBag(); // get all shoping bag
         await this.getCategories();   //  get all categories
         await this.setState({
-            currentDate :  GetToday('shamsi')
+            currentDate: GetToday('shamsi')
         })
         this.insertParam('start_date', GetToday('shamsi'))
         this.insertParam('end_date', GetToday('shamsi'))
- 
-       console.log(this.state.categories)
+
+        console.log(this.state.categories)
 
     }
 
@@ -89,12 +89,12 @@ class CreateTicket extends Component {
         window.removeEventListener('scroll', this.handleScroll);
     }
 
-    getCurrentDate(){
+    getCurrentDate() {
         let d = new Date();
         let fullDate = d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate()
         //  console.log(fullDate)
         //  console.log(dateMilady(fullDate))
-         return dateMilady(fullDate)
+        return dateMilady(fullDate)
     }
 
 
@@ -149,13 +149,13 @@ class CreateTicket extends Component {
 
     getAllTicket = () => {
 
-        
+
         let start_date = this.getParms('start_date') !== 0 ? this.getParms('start_date') : this.getCurrentDate();
         let end_date = this.getParms('end') !== 0 ? this.getParms('end') : this.getCurrentDate();
 
         //console.log('agency/ticket?categories_id=' + this.getParms('categories') + '&start_date=' + start_date   + '&end_date=' + end_date )
 
-        this.getData('agency/ticket?categories_title=' + this.getParms('categories') + '&start_date=' + start_date   + '&end_date=' + end_date )
+        this.getData('agency/ticket?categories_title=' + this.getParms('categories') + '&start_date=' + start_date + '&end_date=' + end_date)
     }
 
     getData(key) {
@@ -181,11 +181,11 @@ class CreateTicket extends Component {
         })
             .then(response => response.json())
             .then(responsJson => {
-               console.log(responsJson.data.total)
+                console.log(responsJson.data.total)
                 this.setState({
                     allTickets: responsJson.data.total,
                     agentLoading: false,
-                   
+
                 })
             })
     }
@@ -221,12 +221,12 @@ class CreateTicket extends Component {
         })
             .then(response => response.json())
             .then(responsJson => {
-              //  console.log(responsJson.data)
+                //  console.log(responsJson.data)
 
                 this.setState({
                     shopingBag: responsJson.data,
                     getShoping: false,
-                    customerName:  responsJson.data.customer ? responsJson.data.customer.name : '',
+                    customerName: responsJson.data.customer ? responsJson.data.customer.name : '',
                     customerPhone: responsJson.data.customer ? responsJson.data.customer.phone : ''
                 })
 
@@ -279,14 +279,14 @@ class CreateTicket extends Component {
     // get All categories ------------------------------>
     //
 
-    getCategories = async()=> { 
-        await  this.fetchCategories('agency/categories')
+    getCategories = async () => {
+        await this.fetchCategories('agency/categories')
 
 
     }
 
 
-    fetchCategories =async(key) => {
+    fetchCategories = async (key) => {
         const url = base.baseURL + key;
 
         fetch(url, {
@@ -302,15 +302,17 @@ class CreateTicket extends Component {
             referrer: "no-referrer"
         })
             .then(response => response.json())
-            .then( responseJson => {
+            .then(responseJson => {
                 this.setState({
                     categories: responseJson.data
 
                 })
-               // console.log(responseJson.data)
+                // console.log(responseJson.data)
             })
     }
 
+
+    selectHoby = React.createRef()
 
     _selectCategorie = async (item) => {
         await this.setState({
@@ -320,7 +322,23 @@ class CreateTicket extends Component {
         console.log(`selectedt ${item.id}`)
         this.insertParam('categories', item.title);
 
+        this.selectHoby.current.style.display = 'none';
+            
+
     }
+
+
+    // enter and leave select menu
+    enterSelectHoby = () =>{
+        this.selectHoby.current.style.display = 'block'
+    }
+
+    leaveSelectHoby = () =>{
+        this.selectHoby.current.style.display = 'none'
+    }
+
+
+
 
     //
     // insert and get params from url --------------------------------------------------------->
@@ -343,7 +361,7 @@ class CreateTicket extends Component {
         let url = new URL(url_string);
 
         const val = url.searchParams.get(value);
-       // console.log(val)
+        // console.log(val)
         if (val !== null)
             return val;
         return 0
@@ -366,19 +384,19 @@ class CreateTicket extends Component {
         console.log(unix)
         console.log(formatted)
         // console.log("start date ");
-        this.insertParam('start_date',formatted);
+        this.insertParam('start_date', formatted);
     }
 
     changeTimeDate = (unix, formatted) => {
         console.log(unix)
         console.log(formatted)
         // console.log("end date ")
-        this.insertParam('end_date',formatted)
+        this.insertParam('end_date', formatted)
 
     }
 
     DatePickerInput(props) {
-       //  console.log(props)
+        //  console.log(props)
         return <input className="popo" {...props} ></input>;
     }
 
@@ -394,43 +412,43 @@ class CreateTicket extends Component {
     // paymnet to shop ------------------->
     //
 
-    _paymentToShop =async() => {
+    _paymentToShop = async () => {
         // console.log("payment call")
 
         let chechking = false;
 
-        if(this.state.customerName === '' || this.state.customerName === null){
+        if (this.state.customerName === '' || this.state.customerName === null) {
             chechking = true;
             this.setState({
-                customerNameError:'لطفا نام مشتری را وارد نمایید'
+                customerNameError: 'لطفا نام مشتری را وارد نمایید'
             })
         }
 
-        if(this.state.customerPhone === '' || this.state.customerPhone === null){
+        if (this.state.customerPhone === '' || this.state.customerPhone === null) {
             chechking = true;
             this.setState({
-                customerPhoneError:'لطفا شماره مشتری را وارد نمایید'
+                customerPhoneError: 'لطفا شماره مشتری را وارد نمایید'
             })
         }
 
 
-           // provider data for API --------->
-           const data = {
+        // provider data for API --------->
+        const data = {
             "name": this.state.customerName,
             "phone": this.state.customerPhone
-           
+
         }
 
 
-        if(chechking === false){
+        if (chechking === false) {
             console.log('data success cheking');
             const res = await this.postPayment(data, 'agency/shopping');
 
             console.log(res)
             if (res.status === 200) {
-            browserHistory.push({pathname:'/payment-method',state: res.data })
+                browserHistory.push({ pathname: '/payment-method', state: res.data })
             }
-            else{
+            else {
                 console.log(res)
             }
         }
@@ -467,8 +485,7 @@ class CreateTicket extends Component {
                 //console.log(res, data)
                 this.setState({ isLoadingPayment: false })
                 // after add refresh render all agent and show new record in list ....
-                if (res === 200) 
-                {
+                if (res === 200) {
                     console.log(data)
                 }
                 return ({ 'status': res, 'data': data.data })
@@ -482,31 +499,29 @@ class CreateTicket extends Component {
     //
     //
 
-    _changedate = async(type) => {
+    _changedate = async (type) => {
         console.log(this.state.currentDate)
         let date = this.state.currentDate.split('/')
         let day = date[2]
         console.log(date[2])
 
-        if(type === 'min'){
+        if (type === 'min') {
 
-            if(day > 1)
-            {
-                day --;
+            if (day > 1) {
+                day--;
                 console.log(day)
             }
 
-        } 
+        }
         else {
-            if(day < 30)
-            {
-                day ++;
+            if (day < 30) {
+                day++;
                 console.log(day)
             }
         }
 
 
-       await this.setState({
+        await this.setState({
             currentDate: date[0] + '/' + date[1] + '/' + day
         })
 
@@ -522,14 +537,14 @@ class CreateTicket extends Component {
 
         const renderAllTickets = (
             // render all agents and pass props name , avatar , level ------->
-            
+
             this.state.agentLoading === false ? this.state.allTickets.map((item, index) =>
-                
+
                 <Tickets key={index}
                     id={item.id}
                     title={item.title}
                     data={item}
-                    action={() => this.getAllShopingBag()} /> 
+                    action={() => this.getAllShopingBag()} />
 
             ) : <div className="loader"></div>
 
@@ -539,15 +554,15 @@ class CreateTicket extends Component {
         const renderShopingBag = (
             // render all agents and pass props name , avatar , level ------->
             this.state.getShoping === false ? this.state.shopingBag !== null ? this.state.shopingBag.shoppingBags.map((item, index) =>
-                
+
                 <SmallOrder key={index}
-                    title={item.type === 'product' ? item.products.title : item.tours.title }
+                    title={item.type === 'product' ? item.products.title : item.tours.title}
                     orderNumber={item.type === 'product' ? item.products.title : item.tours.title}
                     // date="شنبه 1397/12/10 سانس 17:45تا 19:45"
-                    date={DateJalaly(timeStamp(item.products_episode.start_date)) +  ' سانس ' + item.products_episode.start_hours + '-' + item.products_episode.end_hours}
+                    date={DateJalaly(timeStamp(item.products_episode.start_date)) + ' سانس ' + item.products_episode.start_hours + '-' + item.products_episode.end_hours}
                     prices={item}
-                    action={() => this.getAllShopingBag()}/>  ): <p>No Data for show!</p>
-                
+                    action={() => this.getAllShopingBag()} />) : <p>No Data for show!</p>
+
 
                 :
                 <div className="loader"></div>
@@ -586,7 +601,7 @@ class CreateTicket extends Component {
                                 </p>
                                 <p className="create-ticket-your-bascket-row2" >
                                     <span>مبلغ کل</span>
-                                    <span>{PriceDigit(this.state.shopingBag.total_price,'price')}</span>
+                                    <span>{PriceDigit(this.state.shopingBag.total_price, 'price')}</span>
                                 </p>
                                 <p className="create-ticket-your-bascket-row2" >
                                     <span>تعداد سفارشات</span>
@@ -608,37 +623,37 @@ class CreateTicket extends Component {
                                     <div className="create-ticket-customer-factor" >
                                         <p className="create-ticket-customer-factor-row">
                                             <span>فاکتور مشتری</span>
-                                            <span style={{ color: '#FF3131' }}>{PriceDigit(this.state.shopingBag.total_price,'price')} تومان</span>
+                                            <span style={{ color: '#FF3131' }}>{PriceDigit(this.state.shopingBag.total_price, 'price')} تومان</span>
                                         </p>
                                         <p className="create-ticket-customer-factor-row">
                                             <span >درآمد مجموعه از این فاکتور</span>
-                                            <span style={{ color: '#00BF66' }}>{PriceDigit(this.state.shopingBag.agencyProfitTotal ,'price')} تومان</span>
+                                            <span style={{ color: '#00BF66' }}>{PriceDigit(this.state.shopingBag.agencyProfitTotal, 'price')} تومان</span>
                                         </p>
                                         <p className="create-ticket-customer-factor-row">
                                             <span>درآمد شما از این فاکتور</span>
-                                            <span style={{ color: '#00BF66' }}>{PriceDigit(this.state.shopingBag.agencyAgentProfitTotal,'price')} تومان</span>
+                                            <span style={{ color: '#00BF66' }}>{PriceDigit(this.state.shopingBag.agencyAgentProfitTotal, 'price')} تومان</span>
                                         </p>
                                     </div>
                                     <div className="create-ticket-customer-factor-total">
                                         <span>مبلغ قابل پرداخت شما</span>
-                                        <span style={{ color: '#00BF66' }}>{PriceDigit(this.state.shopingBag.total_price,'price')} تومان</span>
+                                        <span style={{ color: '#00BF66' }}>{PriceDigit(this.state.shopingBag.total_price, 'price')} تومان</span>
                                     </div>
                                 </div>
                                 <div className="create-ticket-transaction" >
-                                    <Input 
+                                    <Input
                                         name="customerName"
-                                        val={this.state.customerName || ''} 
+                                        val={this.state.customerName || ''}
                                         placeholder="نام خریدار"
                                         changed={this.changedHandler}
                                         error={this.state.customerNameError}
-                                     />
-                                    <Input 
-                                        name="customerPhone" 
+                                    />
+                                    <Input
+                                        name="customerPhone"
                                         type="number"
                                         placeholder="شماره تماس خریدار "
                                         val={this.state.customerPhone}
                                         changed={this.changedHandler}
-                                        error={this.state.customerPhoneError} 
+                                        error={this.state.customerPhoneError}
                                     />
                                     <div onClick={() => this._paymentToShop()} className="create-ticket-pay" >
                                         <div className="create-ticket-imgs">
@@ -648,8 +663,8 @@ class CreateTicket extends Component {
                                         <div className="create-ticket-text">
                                             <span>پرداخت و صدور بلیت</span> {this.state.isLoadingPayment ? <div className="loader-button"></div> : ''}
                                         </div>
-                                        
-                                        
+
+
                                     </div>
                                 </div>
                             </div>
@@ -701,7 +716,7 @@ class CreateTicket extends Component {
                                     <div className="date-select-create-ticket">
                                         <DatePicker
                                             format="jYYYY/jMM/jDD"
-                                            onChangeStart={this.change}  
+                                            onChangeStart={this.change}
                                             inputComponent={this.DatePickerInput}
                                             placeholder={this.state.currentDate}
                                             id="rangePickerStart"
@@ -716,12 +731,12 @@ class CreateTicket extends Component {
                                 </div>
                             </div>
                             <div className="create-ticket-search">
-                                <div className="create-ticket-features" >
-                                     <span className="create-ticket-features-right" >
+                                <div className="create-ticket-features" onMouseEnter={this.enterSelectHoby} onMouseLeave={this.leaveSelectHoby}>
+                                    <span className="create-ticket-features-right" >
                                         <span>{this.state.categorie || 'همه'} </span>
                                     </span>
                                     <img src={arrowdown2} alt="فلش" />
-                                    <ul className="create-ticket-features-lists" >
+                                    <ul className="create-ticket-features-lists" ref={this.selectHoby} >
                                         <li className="create-ticket-features-list" >
                                             <ul className="create-ticket-features-col" >
                                                 {renderCategory}
@@ -738,9 +753,9 @@ class CreateTicket extends Component {
 
 
                         {/* --------------------------------------------------------------- */}
-                       
-                        {renderAllTickets } 
-                        
+
+                        {renderAllTickets}
+
                         {/* {this.state.allTickets.length === 0 ? notFound : renderAllTickets}
                         {console.log(this.state.allTickets.length )}
                         {console.log(this.state.allTickets ? 'notFound' : 'renderAllTickets')} */}
